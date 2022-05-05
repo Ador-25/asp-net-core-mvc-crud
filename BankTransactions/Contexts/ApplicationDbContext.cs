291ea0e44622cc85a91a33eb6ad1780ba5589033
Abtahee Farhan Ador <abtahee.ador@northsouth.edu>
2022-05-06 02:21:37 +0600

    initial commit

---
+++ BankTransactions/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BankTransactions.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BankTransactions.Contexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ BankTransactions/Program.cs
@@ -0,0 +1,37 @@
+using BankTransactions.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//DI for DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BankAppConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+//
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+// install packages -efcore,efcore tools,sql server
+// model classes - transaction
+//context class
+//migration
+//add controller -> mvc.controller-> mvc.ctr with views & ef core (****use code generation package)

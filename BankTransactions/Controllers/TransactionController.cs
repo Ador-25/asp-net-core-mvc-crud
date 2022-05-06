ef41ca7342c275d1f81e10b714ad547757768c25
Abtahee Farhan Ador <abtahee.ador@northsouth.edu>
2022-05-07 02:52:23 +0600

    views added and code generation fixed

--- BankTransactions/Controllers/TransactionController.cs
+++ BankTransactions/Controllers/TransactionController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BankTransactions.Contexts;
+using BankTransactions.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankTransactions.Controllers
 {
     public class TransactionController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit()
+        {
+            return View(new Transaction());
+        }
+
+        // POST: Transaction/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                {
+                    transaction.Date = DateTime.Now;
+                    _context.Add(transaction);
+                }
+                else
+                    _context.Update(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
